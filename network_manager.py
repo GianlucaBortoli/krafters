089dfce88f45b2c42bc004eaee87f54ff8ab7e6b
M4rc0SX <federicimarcosx@gmail.com>
2016-06-13 19:26:45 +0200

    netem_manager debugged, interaction with test_executor fixed

--- network_manager.py
+++ network_manager.py
@@ -50,7 +50,7 @@ class NetemManager:
     modify_outgoing_connections_commands = ["sudo tc qdisc change dev %INTERFACE% parent 1:%PID%2 netem %NETEM%"]
 
     def __init__(self, interface, host, peers):
-        logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+        logging.basicConfig(filename=('debug'+str(host["id"]))+'.log', level=logging.DEBUG)
         self.interface = str(interface)
         self.host = host
         self.peers = peers
@@ -75,82 +75,103 @@ class NetemManager:
         return error
 
     def create_root_qdisc(self):
-        err = self.run_commands(self.create_root_commands, {self.interface_token: self.interface})
-        if err:
-            logging.error("error creating root!\n\t" + err)
+        print("Creating root")
+        try:
+            err = self.run_commands(self.create_root_commands, {self.interface_token: self.interface})
+            if err:
+                logging.error("error creating root!\n\t" + err)
+                return False
+            else:
+                return True
+        except:
             return False
-        else:
-            return True
 
     def clean_all_qdisc(self):
-        err = self.run_commands(self.delete_root_commands, {self.interface_token: self.interface})
-        if err:
-            logging.error("error removing root!\n\t" + err)
+        print("Cleaning all qdiscs")
+        try:
+            err = self.run_commands(self.delete_root_commands, {self.interface_token: self.interface})
+            if err:
+                logging.error("error removing root!\n\t" + err)
+                return False
+            else:
+                return True
+        except:
             return False
-        else:
-            return True
 
     def create_peer_qdisc(self, peer):
-        id = str(self.host["id"])
-        qid = str(peer["id"])
-        source_id = id + qid + "1"
-        destination_id = id + qid + "2"
-        peer_port = str(peer["port"])
-        peer_address = str(peer["address"])
-        err = self.run_commands(self.create_peer_class_commands, {self.interface_token: self.interface,
-                                                                  self.source_id_token: source_id,
-                                                                  self.destination_id_token: destination_id})
-        if err:
-            logging.error("error creating class for peer " + str(peer) + "!\n\t" + err)
-            return False
-        else:
-            err = self.run_commands(self.create_peer_qdisc_commands, {self.interface_token: self.interface,
+        try:
+            id = str(self.host["id"])
+            qid = str(peer["id"])
+            source_id = id + qid + "1"
+            destination_id = id + qid + "2"
+            peer_port = str(peer["port"])
+            peer_address = str(peer["address"])
+            err = self.run_commands(self.create_peer_class_commands, {self.interface_token: self.interface,
                                                                       self.source_id_token: source_id,
                                                                       self.destination_id_token: destination_id})
             if err:
-                logging.error("error creating qdisc for peer " + str(peer) + "!\n\t" + err)
+                logging.error("error creating class for peer " + str(peer) + "!\n\t" + err)
                 return False
             else:
-                err = self.run_commands(self.create_peer_filter_commands, {self.interface_token: self.interface,
-                                                                           self.source_id_token: source_id,
-                                                                           self.destination_id_token: destination_id,
-                                                                           self.peer_port_token: peer_port,
-                                                                           self.peer_address_token: peer_address,
-                                                                           self.host_port_token: str(self.host["port"]),
-                                                                           self.host_address_token: str(
-                                                                                   self.host["address"])})
+                err = self.run_commands(self.create_peer_qdisc_commands, {self.interface_token: self.interface,
+                                                                          self.source_id_token: source_id,
+                                                                          self.destination_id_token: destination_id})
                 if err:
-                    logging.error("error creating filter for peer " + str(peer) + "!\n\t" + err)
+                    logging.error("error creating qdisc for peer " + str(peer) + "!\n\t" + err)
                     return False
                 else:
-                    return True
+                    err = self.run_commands(self.create_peer_filter_commands, {self.interface_token: self.interface,
+                                                                               self.source_id_token: source_id,
+                                                                               self.destination_id_token: destination_id,
+                                                                               self.peer_port_token: peer_port,
+                                                                               self.peer_address_token: peer_address,
+                                                                               self.host_port_token: str(self.host["port"]),
+                                                                               self.host_address_token: str(
+                                                                                       self.host["address"])})
+                    if err:
+                        logging.error("error creating filter for peer " + str(peer) + "!\n\t" + err)
+                        return False
+                    else:
+                        return True
+        except:
+            return False
 
     def modify_incoming_connection(self, peer_id, netem_command):
-        err = self.run_commands(self.modify_incoming_connections_commands, {self.interface_token: self.interface,
-                                                                            self.peer_id_token: str(
-                                                                                    self.host["id"]) + peer_id,
-                                                                            self.netem_token: netem_command})
-        if err:
-            logging.error("error modifying incoming connection from peer " + peer_id + "!\n\t" + err)
+        print("Command '"+netem_command+"'to incoming connection received")
+        try:
+            err = self.run_commands(self.modify_incoming_connections_commands, {self.interface_token: self.interface,
+                                                                                self.peer_id_token: str(
+                                                                                        self.host["id"]) + peer_id,
+                                                                                self.netem_token: netem_command})
+            if err:
+                logging.error("error modifying incoming connection from peer " + peer_id + "!\n\t" + err)
+                return False
+            else:
+                return True
+        except:
             return False
-        else:
-            return True
 
     def modify_outgoing_connection(self, peer_id, netem_command):
-        err = self.run_commands(self.modify_outgoing_connections_commands, {self.interface_token: self.interface,
-                                                                            self.peer_id_token: str(
-                                                                                    self.host["id"]) + peer_id,
-                                                                            self.netem_token: netem_command})
-        if err:
-            logging.error("error modifying outgoing connection to peer " + peer_id + "!\n\t" + err)
+        print("Command '"+netem_command+"' to outgoing connection received")
+        try:
+            err = self.run_commands(self.modify_outgoing_connections_commands, {self.interface_token: self.interface,
+                                                                                self.peer_id_token: str(
+                                                                                        self.host["id"]) + peer_id,
+                                                                                self.netem_token: netem_command})
+            if err:
+                logging.error("error modifying outgoing connection to peer " + peer_id + "!\n\t" + err)
+                return False
+            else:
+                return True
+        except:
             return False
-        else:
-            return True
 
     def modify_connection(self, peer_id, netem_command):
+        print("Command '"+netem_command+"' received")
         return self.modify_incoming_connection(peer_id, netem_command) and self.modify_outgoing_connection(peer_id, netem_command)
 
     def init_qdisc(self):
+        print("Initializing peer qdiscs")
         result = True
         for peer in self.peers:
             result = result and self.create_peer_qdisc(peer)
